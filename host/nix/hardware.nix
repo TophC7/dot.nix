d51f8a1cb4109a2fdeeceaf05e0354cb8d8db066
Chris Toph <toph@ryot.foo>
2024-09-25 22:59:02 -0400

    mounted SSHFS Pool @nix

--- host/nix/hardware.nix
+++ host/nix/hardware.nix
@@ -1,7 +1,23 @@
 {
-  # TODO: SSHFS
-  # fileSystems."/" = {
-  #   device = "/dev/sda1";
-  #   fsType = "ext4";
-  # };
+  programs.fuse.userAllowOther = true;
+
+  fileSystems = {
+    "/pool" = {
+      device = "toph@104.40.4.24:/pool";
+      fsType = "sshfs";
+      options = [
+        "defaults"
+        "reconnect"
+        "_netdev"
+        "allow_other"
+        "identityfile=/home/toph/.ssh/pve"
+      ];
+    };
+
+    "/home/toph/git" = {
+      fsType = "none";
+      device = "/pool/git";
+      options = ["bind" "nofail"];
+    };
+  };
 }
