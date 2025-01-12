91ec1b141a2dfe4c4b1ada157aee13867c953dd5
Chris Toph <toph@ryot.foo>
2025-01-12 14:19:40 -0500

    komodo binds symlinks

--- host/komodo/home/default.nix
+++ host/komodo/home/default.nix
@@ -1,17 +1,22 @@
-{ pkgs, ... }:
-{  
+{ pkgs, config, ... }:
+{
   # Module imports
   imports = [
     # Common Modules
-      ../../../common/home
+    ../../../common/home
   ];
 
+  home.file = {
+    Pool.source = config.lib.file.mkOutOfStoreSymlink "/pool";
+    DockerStorage.source = config.lib.file.mkOutOfStoreSymlink "/mnt/DockerStorage";
+  };
+
   home.packages = with pkgs; [
-      fastfetch
-      fish
-      fishPlugins.grc
-      fishPlugins.tide
-      grc
-      lazydocker
-    ];
+    fastfetch
+    fish
+    fishPlugins.grc
+    fishPlugins.tide
+    grc
+    lazydocker
+  ];
 }
