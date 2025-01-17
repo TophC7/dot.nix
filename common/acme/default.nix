27790e446ca08b1eb40de3df3b72c24cf9bc5679
Chris Toph <toph@ryot.foo>
2025-01-17 01:18:35 -0500

    Added all my domains to acme

--- common/acme/default.nix
+++ common/acme/default.nix
@@ -1,18 +1,39 @@
-{ config, lib, pkgs, ... }: 
 {
-    
-    # letsencrypt 
-    security.acme = {
-        acceptTerms = true;
-        defaults = {
-            email = "chris@toph.cc";
-            dnsProvider = "cloudflare";
-            environmentFile = ./cloudflare.ini;
-        };
-        certs = {
-            "ryot.foo" = {
-                extraDomainNames = ["*.ryot.foo"];
-            };
-        };
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+
+  # letsencrypt
+  security.acme = {
+    acceptTerms = true;
+    defaults = {
+      email = "chris@toph.cc";
+      dnsProvider = "cloudflare";
+      environmentFile = ./cloudflare.ini;
     };
+    certs = {
+      "goldenlemon.cc" = {
+        extraDomainNames = [ "*.goldenlemon.cc" ];
+      };
+
+      # "kwahson.com" = {
+      #   extraDomainNames = [ "*.kwahson.com" ];
+      # };
+
+      # "kwahson.xyz" = {
+      #   extraDomainNames = [ "*.kwahson.xyz" ];
+      # };
+
+      # "toph.cc" = {
+      #   extraDomainNames = [ "*.toph.cc" ];
+      # };
+
+      "ryot.foo" = {
+        extraDomainNames = [ "*.ryot.foo" ];
+      };
+    };
+  };
 }
