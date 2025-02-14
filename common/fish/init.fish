9ba2128a6e2e5fb19cd4f9ef5709a9e51f7794fa
Chris Toph <toph@ryot.foo>
2025-02-14 14:52:10 -0500

    fish function to clean nix

--- common/fish/init.fish
+++ common/fish/init.fish
@@ -191,6 +191,13 @@ function s
     ssh (whoami)@$argv
 end
 
+function garbage
+    sudo nix-collect-garbage --delete-old
+    nix-collect-garbage --delete-old
+    sudo nix-store --gc
+    nix-store --gc
+end
+
 set fish_greeting # Disable greeting
 
 fastfetch
