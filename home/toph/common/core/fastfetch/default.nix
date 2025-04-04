2befbf3aa25cc2f674ea09b16245b0325ff50e64
Chris Toph <toph@ryot.foo>
2025-04-04 17:19:17 -0400

    New Fastfetch prompt with weather script and fancy title

--- home/toph/common/core/fastfetch/default.nix
+++ home/toph/common/core/fastfetch/default.nix
@@ -9,6 +9,8 @@
     let
       hostname = config.hostSpec.hostName;
       logoFile = ./. + "/host/${hostname}.txt";
+      weather = import ./scripts/weather.nix { inherit pkgs; };
+      title = import ./scripts/title.nix { inherit pkgs; };
     in
     {
       enable = true;
@@ -24,71 +26,146 @@
           };
         };
         display = {
-          separator = " ";
+          bar = {
+            borderLeft = "⦉";
+            borderRight = "⦊";
+            charElapsed = "⏹";
+            charTotal = "⬝";
+            width = 10;
+          };
+          percent = {
+            type = 2;
+          };
+          separator = "";
         };
+        # modules = [
+        #   {
+        #     type = "colors";
+        #     symbol = "square";
+        #   }
+        #   "break"
+        #   {
+        #     key = "{#31} ┤ user »{#keys}";
+        #     type = "title";
+        #     format = "{user-name}";
+        #   }
+        #   {
+        #     key = "{#32} ┤ host »{#keys}";
+        #     type = "title";
+        #     format = "{host-name}";
+        #   }
+        #   {
+        #     key = "{#33} ┤ uptime »{#keys}";
+        #     type = "uptime";
+        #   }
+        #   {
+        #     key = "{#34} ┤ distro »{#keys}";
+        #     type = "os";
+        #   }
+        #   {
+        #     key = "{#36} ┤ desktop »{#keys}";
+        #     type = "de";
+        #   }
+        #   {
+        #     key = "{#32} ┤ shell »{#keys}";
+        #     type = "shell";
+        #   }
+        #   {
+        #     key = "{#33} ┤ cpu »{#keys}";
+        #     type = "cpu";
+        #     showPeCoreCount = true;
+        #   }
+        #   {
+        #     key = "{#34} ┤ disk »{#keys}";
+        #     type = "disk";
+        #     folders = "/";
+        #   }
+        #   {
+        #     key = "{#35} ┤ memory »{#keys}";
+        #     type = "memory";
+        #   }
+        #   {
+        #     key = "{#36} ┤ network »{#keys}";
+        #     type = "localip";
+        #     format = "{ipv4} ({ifname})";
+        #   }
+        #   "break"
+        #   {
+        #     type = "colors";
+        #     symbol = "square";
+        #   }
+        # ];
         modules = [
+          "break"
           {
-            key = "╭───────────╮";
-            type = "custom";
+            key = " ";
+            shell = "fish";
+            text = "fish ${title}";
+            type = "command";
+          }
+          "break"
+          {
+            key = "weather » {#keys}";
+            keyColor = "1;97";
+            shell = "${pkgs.fish}/bin/fish";
+            text = "fish ${weather} 'Richmond'";
+            type = "command";
           }
           {
-            key = "│ {#31} user    {#keys}│";
-            type = "title";
-            format = "{user-name}";
+            key = "cpu     » {#keys}";
+            keyColor = "1;31";
+            showPeCoreCount = true;
+            type = "cpu";
           }
           {
-            key = "│ {#32}󰇅 host    {#keys}│";
-            type = "title";
-            format = "{host-name}";
+            format = "{0} ({#3;32}{3}{#})";
+            key = "wm      » {#keys}";
+            keyColor = "1;32";
+            type = "wm";
           }
           {
-            key = "│ {#33}󰅐 uptime  {#keys}│";
+            text = "printf '%s%s' (string upper (string sub -l 1 $SHELL)) (string lower (string sub -s 2 $SHELL))";
+            key = "shell   » {#keys}";
+            keyColor = "1;33";
+            type = "command";
+            shell = "${pkgs.fish}/bin/fish";
+          }
+          {
+            key = "uptime  » {#keys}";
+            keyColor = "1;34";
             type = "uptime";
           }
           {
-            key = "│ {#34}{icon} distro  {#keys}│";
-            type = "os";
-          }
-          {
-            key = "│ {#36}󰇄 desktop {#keys}│";
-            type = "de";
-          }
-          {
-            key = "│ {#32} shell   {#keys}│";
-            type = "shell";
-          }
-          {
-            key = "│ {#33}󰍛 cpu     {#keys}│";
-            type = "cpu";
-            showPeCoreCount = true;
-          }
-          {
-            key = "│ {#34}󰉉 disk    {#keys}│";
-            type = "disk";
             folders = "/";
+            format = "{0~0,-4} / {2} {13}";
+            key = "disk    » {#keys}";
+            keyColor = "1;35";
+            type = "disk";
           }
           {
-            key = "│ {#35} memory  {#keys}│";
+            format = "{0~0,-4} / {2} {4}";
+            key = "memory  » {#keys}";
+            keyColor = "1;36";
             type = "memory";
           }
           {
-            key = "│ {#36}󰩟 network {#keys}│";
+            format = "{ipv4~0,-3} ({#3;32}{ifname}{#})";
+            key = "network » {#keys}";
+            keyColor = "1;37";
             type = "localip";
-            format = "{ipv4} ({ifname})";
           }
           {
-            key = "├───────────┤";
-            type = "custom";
+            format = "{artist} - {title} ({#3;32}{6}{#})";
+            key = "media   » {#keys}";
+            keyColor = "5;92";
+            type = "media";
           }
+          "break"
           {
-            key = "│ {#39} colors  {#keys}│";
+            symbol = "square";
             type = "colors";
-            symbol = "circle";
           }
-          {
-            key = "╰───────────╯";
-            type = "custom";
-          }
+          "break"
         ];
       };
     };
