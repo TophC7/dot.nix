dc34a785f77fd227622ab1111454f234a0108d18
Chris Toph <toph@ryot.foo>
2025-06-16 23:52:40 -0400

    Adds dev shell in flake and removes old shell.nix

--- flake.nix
+++ flake.nix
@@ -166,5 +166,71 @@
         in
         customPackages
       );
+
+      ## SHELL ##
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ self.overlays.default ];
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            nativeBuildInputs =
+              with pkgs;
+              [
+                # Basic nix tools
+                nix
+                nixos-rebuild
+                home-manager
+                nh
+                # Git and git-crypt
+                git
+                git-crypt
+                gnupg
+                gpg-tui
+                # Shells
+                fish
+                bash
+                # Config tools
+                dconf2nix
+                # Network tools
+                curl
+                wget
+                # System tools
+                coreutils
+                findutils
+                gzip
+                zstd
+                # Text editors
+                micro
+                # microsoft-edit
+                # Diagnostics
+                inxi
+                pciutils
+                usbutils
+                lshw
+              ]
+              ++ [
+                inputs.yay.packages.${system}.default # My yay.nix tool
+              ];
+
+            NIX_CONFIG = "experimental-features = nix-command flakes";
+
+            shellHook = ''
+              clear
+              echo "Development shell initialized"
+              echo -e "Run '\033[1;34myay rebuild\033[0m' to rebuild your system"
+
+              # Set FLAKE to the current working directory
+              export FLAKE="$PWD"
+              echo -e "FLAKE environment variable is set to: \033[1;34m$FLAKE\033[0m"
+            '';
+          };
+        }
+      );
     };
 }
