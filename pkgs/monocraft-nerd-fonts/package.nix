83702b2f90a23bddd72e4f92163fa6eb36bc0b3f
Chris Toph <toph@ryot.foo>
2025-09-10 16:58:38 -0400

    Chore(pkgs): Update monocraft version to 4.1

--- pkgs/monocraft-nerd-fonts/package.nix
+++ pkgs/monocraft-nerd-fonts/package.nix
@@ -1,12 +1,12 @@
 { pkgs, lib, ... }:
 pkgs.stdenvNoCC.mkDerivation {
   pname = "monocraft-nerd-fonts";
-  version = "4.0";
+  version = "4.1";
 
   phases = [ "installPhase" ]; # Removes all phases except installPhase
 
   src = pkgs.fetchurl {
-    url = "https://github.com/IdreesInc/Monocraft/releases/download/v4.0/Monocraft-nerd-fonts-patched.ttc";
+    url = "https://github.com/IdreesInc/Monocraft/releases/download/v4.1/Monocraft-nerd-fonts-patched.ttc";
     sha256 = "95801bf21826bf8572af3787af82e77ee48df4bfb87e90c4317fcffbe7eaf037";
   };
 
@@ -22,5 +22,4 @@ pkgs.stdenvNoCC.mkDerivation {
     homepage = "https://github.com/IdreesInc/Monocraft";
     platforms = platforms.all;
   };
-
 }
