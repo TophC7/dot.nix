c352b76f6a6812d473544c63ec6a89b512a8df00
Chris Toph <toph@ryot.foo>
2025-10-01 16:48:23 -0400

    Feat(nexus): add FRP client configuration for Pangolin services

---
+++ hosts/x86/nexus/config/pangolin/frp-client.nix
@@ -0,0 +1,64 @@
+{ config, ... }:
+let
+  frp-token = config.secretsSpec.api.frp;
+in
+{
+  services.frp = {
+    enable = true;
+    role = "client";
+    settings = {
+      serverAddr = "caenus"; # TODO: use ip
+      serverPort = 4040;
+      auth = {
+        method = "token";
+        token = frp-token;
+      };
+
+      # Pangolin service proxies
+      proxies = [
+        {
+          name = "pangolin-http";
+          type = "tcp";
+          localIP = "127.0.0.1";
+          localPort = 80;
+          remotePort = 80;
+        }
+        {
+          name = "pangolin-https";
+          type = "tcp";
+          localIP = "127.0.0.1";
+          localPort = 443;
+          remotePort = 443;
+        }
+        {
+          name = "pangolin-ssh";
+          type = "tcp";
+          localIP = "127.0.0.1";
+          localPort = 222;
+          remotePort = 222;
+        }
+        {
+          name = "pangolin-wireguard";
+          type = "udp";
+          localIP = "127.0.0.1";
+          localPort = 51820;
+          remotePort = 51820;
+        }
+        {
+          name = "pangolin-tunnels";
+          type = "udp";
+          localIP = "127.0.0.1";
+          localPort = 21820;
+          remotePort = 21820;
+        }
+        {
+          name = "pangolin-minecraft";
+          type = "tcp";
+          localIP = "127.0.0.1";
+          localPort = 25565;
+          remotePort = 25565;
+        }
+      ];
+    };
+  };
+}
