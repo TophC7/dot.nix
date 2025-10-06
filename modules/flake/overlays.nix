b80efcbb03dc9558012ecd651ae16f8e60787c27
Chris Toph <toph@ryot.foo>
2025-10-06 16:30:30 -0400

    refactor: consolidate overlays configuration by merging into flake and removing default overlays/ folder

--- modules/flake/overlays.nix
+++ modules/flake/overlays.nix
@@ -1,7 +1,50 @@
+# Defines overlays/custom modifications to upstream packages
 { inputs, lib, self, ... }:
 let
   customLib = import (self.outPath + "/lib") { inherit lib; };
+
+  # Adds custom packages from pkgs directory
+  additions = final: prev:
+    let
+      packages = prev.lib.packagesFromDirectoryRecursive {
+        callPackage = prev.lib.callPackageWith final;
+        directory = customLib.relativeToRoot "pkgs";
+      };
+    in
+    packages;
+
+  # Linux-specific modifications
+  linuxModifications = final: prev:
+    prev.lib.optionalAttrs prev.stdenv.isLinux { };
+
+  # General modifications to existing packages
+  modifications = final: prev: {
+    # Add any package overrides here
+  };
+
+  # Stable channel packages
+  stable-packages = final: _prev: {
+    stable = import inputs.nixpkgs-stable {
+      inherit (final) system;
+      config.allowUnfree = true;
+    };
+  };
+
+  # Unstable channel packages
+  unstable-packages = final: _prev: {
+    unstable = import inputs.nixpkgs-unstable {
+      inherit (final) system;
+      config.allowUnfree = true;
+    };
+  };
 in
 {
-  flake.overlays = import (customLib.relativeToRoot "overlays") { inherit inputs; };
+  flake.overlays = {
+    default = final: prev:
+      (additions final prev)
+      // (modifications final prev)
+      // (linuxModifications final prev)
+      // (stable-packages final prev)
+      // (unstable-packages final prev);
+  };
 }
