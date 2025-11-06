a40c2af4f935004f3eb6026487c9ec929be4f57c
Chris Toph <toph@ryot.foo>
2025-11-05 23:59:35 -0500

    feat: add geometry settings for Niri window rules

--- home/global/common/desktop/niri/window.nix
+++ home/global/common/desktop/niri/window.nix
@@ -9,6 +9,16 @@
   programs.niri = {
     settings = {
       window-rules = [
+        {
+          geometry-corner-radius = {
+            top-left = 8.0;
+            top-right = 8.0;
+            bottom-left = 8.0;
+            bottom-right = 8.0;
+          };
+          clip-to-geometry = true;
+          draw-border-with-background = false;
+        }
         # Code editor
         {
           matches = [
