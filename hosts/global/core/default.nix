3bac0b32bf8d2d80b5cf222cc92d3abd32988ad6
Chris Toph <toph@ryot.foo>
2025-11-14 18:13:01 -0500

    feat(nimbus): add nix-serve configuration with garbage collection

--- hosts/global/core/default.nix
+++ hosts/global/core/default.nix
@@ -6,6 +6,7 @@
   host,
   lib,
   pkgs,
+  secrets,
   ...
 }:
 let
@@ -135,11 +136,13 @@ in
       substituters = [
         "https://cache.nixos.org"
         "https://chaotic-nyx.cachix.org"
+        "https://cache.ryot.foo"
       ];
 
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+        secrets.service.cache.pub
       ];
     };
   };
